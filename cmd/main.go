2c9d20f34bc726c713a15d8a5a4d2ad6441acad0
kevin <snowdenxedward@gmail.com>
2025-09-07 12:00:00 +0530

    chore: update code file system

--- cmd/main.go
+++ cmd/main.go
@@ -4,21 +4,27 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	
+	"github.com/btwkevin/kit/command"
 )
 
 func main() {
 	args := os.Args
-	command := args[1]
+	cmd := args[1]
 	if len(args) < 2 {
-		fmt.Fprintln(os.Stderr, "No command provided")
+		fmt.Fprintln(os.Stderr, "No cmd provided")
 		os.Exit(1)
 	}
 
-	switch command {
+	switch cmd {
 	case "init":
 		CreateGitDirectory()
+	case "cat-file":
+		command.HandleCatFileCommand()
+	case "hash-object":
+		HandleHashObjectCommand()
 	default:
-		fmt.Fprintf(os.Stderr, "Unknown command %s", command)
+		fmt.Fprintf(os.Stderr, "Unknown command %s", cmd)
 		os.Exit(1)
 	}
 }
@@ -37,3 +43,7 @@ func CreateGitDirectory() {
 	fmt.Printf("Initialized empty Git repository in %s.git/\n", path)
 }
 
+// 9da8d6211e77c8a4dd43d929a769ded98592ec92
+func HandleHashObjectCommand (){
+	
+}
